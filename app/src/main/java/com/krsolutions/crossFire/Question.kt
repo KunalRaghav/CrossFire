9dbb8416af6fcac67336fb81242b814e456bc4e1
Aniketh Varma <aaniketh_varma@yahoo.com>
2021-10-02 20:12:49 +0530

    Added new question

--- app/src/main/java/com/krsolutions/crossFire/Question.kt
+++ app/src/main/java/com/krsolutions/crossFire/Question.kt
@@ -45,6 +45,11 @@ object QuestionsBank {
             "People may sneeze or cough while sleeping deeply.",
             false,
             "We can’t sneeze or cough when in deep sleep. Our body must enter a state of wakefulness to do so."
+        ),
+        Question(
+            "Matches were invented before lighters.",
+            false,
+            "The first push-button lighter, the Döbereiner Lamp, was invented in 1824. The practical friction matches similar to what we use today were invented in 1827."
         )
     )
 
